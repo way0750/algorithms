b5d56031cc7af936c615b01aa59011d841fa8cf3
way0750 <bruce0750@gmail.com>
2016-04-27 12:25:25 -0700

    add asyn functions to asynMap

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -75,10 +75,22 @@ asyncMap([
    setTimeout(function(){
      cb('two');
    }, 0);
- }
+ },
+ function(cb){
+   setTimeout(function(){
+     cb('three');
+   }, 200);
+ },
+ function(cb){
+   setTimeout(function(){
+     cb('four');
+   }, 0);
+ },
 ],
  function(results){
    // the results array will equal ['one','two'] even though
    // the second function had a shorter timeout.
    console.log(results); // ['one', 'two']
 });
+
+
