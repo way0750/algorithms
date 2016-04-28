c4cb2a4ea73794efdab996ee56b8e698346e6e88
way0750 <bruce0750@gmail.com>
2016-04-28 13:35:15 -0700

    binarySearch done

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -16,26 +16,49 @@
 // [a,b,c,d,e]
 // 0,1,2,3,4
 // min = 0, max=4, mid = 2
-// 
-// 
-var binarySearch = function (array, target) {
-  var minIndex = 0, maxIndex = array.length-1, midIndex = Math.floor(array.length/2);
 
-  while (minIndex !== maxIndex){
-    var curValue = array[midIndex];
-    if (curValue === target){
-      return midIndex;
-    } else {
-      // compare with midIndex with min and max index
-      if (curValue<target){
-        minIndex = midIndex;
-        midIndex = midIndex + Math.ceil((maxIndex - minIndex)/2);
-      } else {
-        maxIndex = midIndex;
-        midIndex = minIndex + Math.ceil((maxIndex - minIndex)/2);
-      }
+
+
+function binarySearch(array, target) {
+  var min = 0;
+  var max = array.length - 1;
+  while (min <= max){
+    var mid = min + Math.floor( (max - min) / 2 );
+    if (array[mid] === target) {
+      return mid;
+    } else if (array[mid] > target) {
+      max = mid - 1;
+    } else if (array[mid] < target) {
+      min = mid + 1;
     }
   }
   return -1;
+}
 
-};
+var index = binarySearch([1, 2, 3, 4, 5], 4);
+console.log(index); // [3]
+
+
+
+ 
+// var binarySearch = function (array, target) {
+//   var minIndex = 0, maxIndex = array.length-1, midIndex = Math.floor(array.length/2);
+
+//   while (minIndex !== maxIndex){
+//     var curValue = array[midIndex];
+//     if (curValue === target){
+//       return midIndex;
+//     } else {
+//       // compare with midIndex with min and max index
+//       if (curValue<target){
+//         minIndex = midIndex;
+//         midIndex = midIndex + Math.ceil((maxIndex - minIndex)/2);
+//       } else {
+//         maxIndex = midIndex;
+//         midIndex = minIndex + Math.ceil((maxIndex - minIndex)/2);
+//       }
+//     }
+//   }
+//   return -1;
+
+// };
