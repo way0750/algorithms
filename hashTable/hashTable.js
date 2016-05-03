c3cdf1274f89fb2b081c43655cb5ff5c53101492
way0750 <bruce0750@gmail.com>
2016-05-03 03:10:24 -0700

    hashTable first draft done

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -1,77 +1,94 @@
-/**
- * Create a hash table with `insert()`, `retrieve()`, and `remove()` methods.
- * The hashtable does not need to resize but it should still handle collisions.
- */
+// linkedList
+// need to have pointer pointing to 
+// insertToEnd
+// insertToHead
+// removeFromEnd
+// removeFromHead
+// size
 
-var makeHashTable = function(){
-  var result = {};
-  var storage = [];
-  var storageLimit = 1000;
+function LinkedListNode (value) {
+  this.value = value;
+  this.next = null;
+}
 
-  result.insert = function(key, value){
-    var hash = getIndexBelowMaxForKey(key, storageLimit);
-    var bracket = storage[hash];
-    if (Array.isArray(bracket)){
-      var alreadyExist = false;
-      for (var i = 0; i < bracket.length; i++){
-        if (bracket[i][0]===key){
-          bracket[i][1] = value;
-          alreadyExist = true;
-        }
-        if (!alreadyExist){
-          bracket.push([key, value]);
-        }
-      }
+function LinkedList () {
+  this.head = null;
+  this.end = null;
+  this.size = 0;
+}
 
+LinkedList.prototype.insertFromEnd = function(value) {
+  var newNode = new LinkedListNode(value);
+  if (this.head === null && this.end === null) {
+    this.head = newNode;
+    this.end = newNode;
+  } else if (this.end === null ) {
+    this.end = newNode;
+  } else {
+    //set old end's next to the newNode, then set newNode as the end
+    this.end.next = newNode;
+    this.end = newNode;
+  }
+  return ++this.size;
+};
+
+LinkedList.prototype.removeFromHead = function() {
+  if (this.head){
+    //if the head and the end are the same node, then reset both
+    if (this.head === this.end) {
+      this.head = null;
+      this.end = null;
     } else {
-      storage[hash] = [[key, value]];
+      this.head = this.head.next;
     }
-  };
+    --this.size;
+  }
 
-  result.retrieve = function(key){
-    var hash = getIndexBelowMaxForKey(key, storageLimit);
-    var bracket = storage[hash];
-    if (bracket){
-      if (bracket.length < 2 ){
-        return bracket[0][1] ? bracket[0][1] : undefined;
-      } else {
-        for (var i = 0; i < bracket.length; i++) {
-          var singleBracket = bracket[i];
-          if (singleBracket[0]===key){
-            return singleBracket[1];
-          }
-        }
-       }
+  return this.size;
+};
+
+LinkedList.prototype.search = function(value) {
+  var curNode = this.head;
+  while (curNode) {
+    if (curNode.value === value){
+      return curNode;
+    } else {
+      curNode = curNode.next;
     }
-  };
+  }
+  return null;
+};
 
-  result.remove = function(key){
-    var hash = getIndexBelowMaxForKey(key, storageLimit);
-    var bracket = storage[hash];
-    if (bracket){
-      if (bracket.length < 2 ){
-        storage[hash] = [];
-      } else {
-        for (var i = 0; i < bracket.length; i++) {
-          storage[hash] = storage[hash].reduce(function(newBracket, pair){
-            if (pair[0] !== key){newBracket.push(pair);}
-            return newBracket;
-          }, []);
+LinkedList.prototype.findAndRemove = function(value) {
+  // it has to have two point just so you can rejoin a broken list
+  if (this.head && this.head.value === value){
+    var removed = this.head;
+    this.removeFromHead();
+    return removed;
+  } else {
+    var firstNode = this.head, secondNode = (this.head || {}).next;
+    while (secondNode) {
+      if (secondNode.value === value) {
+        firstNode.next = secondNode.next;
+        secondNode.next = null;
+        this.size--;
+        if (this.size === 1) {
+          this.end = this.head;
         }
+        return secondNode;
+      } else {
+        firstNode = secondNode;
+        secondNode = secondNode.next;
       }
-    }    
-  };
-
-  return result;
+    }
+  }
+  return null;
 };
 
 
-
-
-
-// This is a "hashing function". You don't need to worry about it, just use it
-// to turn any string into an integer that is well-distributed between
-// 0 and max - 1
+// ///////////////////////////////////
+// hashTable:
+// Create a hash table with `insert()`, `retrieve()`, and `remove()` methods.
 var getIndexBelowMaxForKey = function(str, max){
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
@@ -81,3 +98,47 @@ var getIndexBelowMaxForKey = function(str, max){
   }
   return hash % max;
 };
+
+
+
+function HashTable (max) {
+  this.storage = [];
+  this.max = max;
+}
+
+HashTable.prototype.insert = function(key, value) {
+  var hashNum = getIndexBelowMaxForKey(key, this.max);
+  this.storage[hashNum] = this.storage[hashNum] || new LinkedList();
+  var list = this.storage[hashNum];
+  var foundEntry = list.search(key);
+  if (foundEntry) {
+    foundEntry.hashItemValue = value;
+  } else {
+    list.insertFromEnd(key);
+    list.end.hashItemValue = value;
+  }
+};
+
+HashTable.prototype.retrieve = function(key) {
+  var hashNum = getIndexBelowMaxForKey(key, this.max);
+  var list = this.storage[hashNum];
+  if (list){
+    return (list.search(key) || {}).hashItemValue;
+  }
+};
+
+HashTable.prototype.remove = function(key) {
+  var hashNum = getIndexBelowMaxForKey(key, this.max);
+  var list = this.storage[hashNum];
+  if (list){
+    var removedEntry = list.findAndRemove(key);
+    if (list.size === 0) {
+      delete this.storage[hashNum];
+    }
+    return removedEntry;
+  }
+};
+
+var hashTable = new HashTable(10);
+hashTable.insert('name', 'way');
+hashTable.retrieve('name');
