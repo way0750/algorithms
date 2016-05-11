0ba169b639ee9af0e7913e9e0ed9cb7fe8bc1930
way0750 <bruce0750@gmail.com>
2016-05-11 00:17:48 -0700

    add algorithm

---
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -0,0 +1,8 @@
+/**
+ * Given an arbitrary input string, return the first nonrepeated character in
+ * the string. For example:
+ *
+ *   firstNonRepeatedCharacter('ABA'); // => 'B'
+ *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
+ */
+
