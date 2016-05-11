6767942e327f9d4e66804127a340ed4dfc9d4cbd
way0750 <bruce0750@gmail.com>
2016-05-11 00:18:05 -0700

    done with nonrepeatedCharacter

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,3 +6,20 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
+
+let firstNonRepeatedCharacter = (string) => {
+  let record = {uniq: []};
+  for (let i = 0; i < string.length; i++){
+    let char = string[i];
+    record[char] = (record[char] || 0) + 1;
+    if (record[char] === 1) {
+      record.uniq.push(char);
+    }
+  }
+
+  return record.uniq.find( (ele) => {
+    return record[ele] === 1;
+  });
+};
+
+firstNonRepeatedCharacter('AACBDB'); // => 'C'
