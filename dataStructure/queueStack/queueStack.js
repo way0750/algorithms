e7b96d48935acab221391b8de68881ab11282534
way0750 <bruce0750@gmail.com>
2016-05-14 02:13:51 -0700

    add queueStack

---
+++ dataStructure/queueStack/queueStack.js
@@ -0,0 +1,6 @@
+/**
+ * Write a stack using your preferred instantiation pattern. 
+ * Avoid using native array methods i.e., push, pop, and length.
+ * Once you're done, implement a queue using two stacks.
+ */
+
