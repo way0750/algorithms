af9fcfb785a1a2d96f6d63cd2549ae8df4c89025
way0750 <bruce0750@gmail.com>
2016-05-13 23:07:07 -0700

    powerSet done

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -16,3 +16,20 @@
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
+
+
+//using recursion:
+//base case: if input string is empty, return [ itself ];
+//how to break smaller: loop from back of string to front and recursively call with str.slice(1);
+//what to do with return: make copy of return array, add str[0] to each and every single one of the element in the copy arr
+//  then concat both arrays together then return
+//what to return: always an array;
+
+var powerSet = (str) => {
+  if (str === '') { return [str]; }
+  return powerSet(str.slice(1)).reduce( (finalSet, strSet) => {
+    return finalSet.concat(strSet, str[0] + strSet);
+  }, []);
+};
+
+powerSet("jump");
