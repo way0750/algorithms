39fe0e39e811ca3cf30d4cf1fa6c6d866ef10339
way0750 <bruce0750@gmail.com>
2016-05-14 02:38:46 -0700

    queue and stack done

--- dataStructure/queueStack/queueStack.js
+++ dataStructure/queueStack/queueStack.js
@@ -4,3 +4,53 @@
  * Once you're done, implement a queue using two stacks.
  */
 
+
+var Stack = function () {
+  this.size = 0;
+  this.stack = {};
+};
+
+Stack.prototype.push = function(val) {
+  this.stack[this.size++] = val;
+  return this.size;
+};
+
+Stack.prototype.pop = function() {
+  if (this.size){
+    --this.size;
+    let val = this.stack[this.size];
+    delete this.stack[this.size];
+    return val;
+  }
+};
+
+let Queue = function () {
+  this.stack1 = new Stack();
+  this.stack2 = new Stack();
+  this.size = 0;
+};
+
+Queue.prototype.push = function(val) {
+  this.size++;
+  this.stack1.push(val);
+  return this.size;
+};
+
+Queue.prototype.shift = function() {
+  if (this.size > 0) {
+    if (this.stack2.size === 0) {
+      while (this.stack1.size) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    this.size--;
+    return this.stack2.pop();
+  }
+};
+
+let q = new Queue();
+q.push(1);
+q.push(3);
+q.push(4);
+q.push(5);
+
