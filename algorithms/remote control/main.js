ea88c9823be989c6e2408797d8e9fbebf3b7808e
way0750 <bruce0750@gmail.com>
2017-02-08 21:42:29 -0800

    add remote control problem

---
+++ algorithms/remote control/main.js
@@ -0,0 +1,17 @@
+/*
+   arrange A-Z into a table with a max width of 5 letters per row
+   IE:
+   A B C D E
+   F G H I J
+   K L M N O
+   P Q R S T
+   U V W X Y
+   Z
+
+   given a string of only A-Z letter(s) generate movements
+   from default starting position at A to each letter
+   ex: if input is GYC
+   then output should be:
+   D!R!DDD!RRR!UUUU!LL!
+*/
+
---
+++ algorithms/remote control/tests.js
@@ -0,0 +1,6 @@
+chai.should();
+describe('Describe a topic, an area of tests. Can be nested', function() {
+  it('this is the actual test', function() {
+    hey().should.be.a('string');
+  });
+});
