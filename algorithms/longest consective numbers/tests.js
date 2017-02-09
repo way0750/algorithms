71592f7af0e260f64e57c93fd67e1c7cc73d20d2
way0750 <bruce0750@gmail.com>
2017-02-08 17:38:42 -0800

    add one more test

--- algorithms/longest consective numbers/tests.js
+++ algorithms/longest consective numbers/tests.js
@@ -5,5 +5,11 @@ describe('findLongestNumSeqImproved', function() {
     let arr = [12, 1, 11, 9, 3, 15, 10, 16, 4, 20, 2, 13, 14];
     let result = findLongestNumSeqImproved(arr);
     result.should.equal(8);
+  });
+
+  it('should return 4 for [1,2,3,5,3]', function() {
+    let arr = [1,2,3,5,3];
+    let result = findLongestNumSeqImproved(arr);
+    result.should.equal(4);
   })
 });
