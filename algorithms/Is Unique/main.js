0c84359a56aeb765b059c6a11983cf7ca8d2f8e1
way0750 <bruce0750@gmail.com>
2017-02-10 13:30:32 -0800

    isUniqe using additional data structure

--- algorithms/Is Unique/main.js
+++ algorithms/Is Unique/main.js
@@ -1,9 +1,31 @@
-/*
-   Is Unique: Implement an algorithm to determine if a string has all unique
-   characters. What if you cannot use additional data structures?
+
+/* Is Unique: Implement an algorithm to determine if a string has all unique
+ * characters. What if you cannot use additional data structures?*/
+
+
+
+
+/* solution one:
+ * use a hashTable to keep track of characters as a for loop goes through the
+ * input string. for each character, add it to the hashTable if not found
+ * if found return false
+ * default return true;
+
+   time complextity: n for adding to hashTable, a for looping
+   so 2n which is n
+   space: n for hashTable
 */
 
 
-let isUnique = (str) => {
-  
+let isUniqueUsingHashTabel = (str) => {
+  let record = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    record[char] = (record[char] || 0) + 1;
+    if (record[char] > 1) {
+      return false;
+    }
+  }
+  return true;
 };
+
--- algorithms/Is Unique/tests.js
+++ algorithms/Is Unique/tests.js
@@ -1,6 +1,10 @@
 chai.should();
 describe('is unique', function() {
   it('return true for abcd ', function() {
-    hey().should.be.a('string');
+    //hey().should.be.a('string');
+    isUnique('abcd').should.be.true;
   });
+  it('return false for aabj', function() {
+    isUnique('aabj').should.be.false;
+  })
 });
