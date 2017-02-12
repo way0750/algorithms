8a72e8ba038b372818d55be0ba24c00020a7b05e
way0750 <bruce0750@gmail.com>
2017-02-11 18:43:33 -0800

    edit testing for zero matrix

--- algorithms/zero matrix/tests.js
+++ algorithms/zero matrix/tests.js
@@ -1,6 +1,6 @@
 chai.should();
 describe('zero matrix', function() {
-  it('works?', function() {
+  it('matrix of 4 by 4', function() {
     let matrix = [
       [1,1,1,1],
       [0,1,1,1],
@@ -12,6 +12,22 @@ describe('zero matrix', function() {
       [0,0,0,0],
       [0,0,0,0],
       [0,0,1,1],
+    ];
+    zeroMatrix(matrix).should.deep.equal(result);
+  });
+
+  it('matrix of 2 by 4', function() {
+    let matrix = [
+      [1,1],
+      [0,1],
+      [0,1],
+      [1,1],
+    ];
+    let result = [
+      [0,1],
+      [0,0],
+      [0,0],
+      [0,1],
     ];
     zeroMatrix(matrix).should.deep.equal(result);
   });
