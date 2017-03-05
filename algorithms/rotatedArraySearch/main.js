494f8db0193a9e89d0d191163432400883b53f9d
way0750 <bruce0750@gmail.com>
2017-03-04 16:58:15 -0800

    rotatedArray concept done

--- algorithms/rotatedArraySearch/main.js
+++ algorithms/rotatedArraySearch/main.js
@@ -14,4 +14,30 @@
  *
  *
  * Target time complexity: O(log(array.length)) O(log(n))
+
+   check and see if the array is actually rotated?
+   if yes then turn it back to a sorted array?
+   that would take n for going throught the array, another n for rotating it
+   then log n for binary search
+   so 2N + logN
+
+   or logN
+
+   another way:
+   straight using binary search
+   if found return the index
+   if not then do one of the following:
+   left side is it sorted and target within it?
+     if yes, then search this portion
+   right side is it sorted and target within it?
+     if yes, then search this portion
+   left side is it not sorted?
+     if yes, then target might be in there search it
+   right side is it not sorted?
+     if yes, then target might be in there search it
+   else return null;
+
+   
+
  */
+
