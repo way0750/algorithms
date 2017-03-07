ef3560d7f1028b57ee34624b807972e2bea10695
way0750 <bruce0750@gmail.com>
2017-03-07 14:35:00 -0800

    quick and dirty test now use local assets

--- codingTest.js
+++ codingTest.js
@@ -1,43 +1,23 @@
 /*
-   I am using my own editor!!!!!!
-   get grand total
-   and make cumulative sum array for sub sum at certain index
-   then go through the cumulative sum array one by one, and compare 
-   grant total - next sum === current sum
-   if yes, then the index is a equalibriliam index
-
+   
 */
 
-function solution(array) {
-  // get grand total:
-  let cumulativeSums = array.reduce(function(arr, num){
-    let lastSum = arr.length ? arr[arr.length - 1] : 0;
-    arr.push(lastSum + num);
-    return arr;
-  }, [0]);
-  let grandTotal = cumulativeSums[cumulativeSums.length - 1];
-  let foundIndex = cumulativeSums.findIndex(function(curSum, index) {
-    let previousSum = cumulativeSums.hasOwnProperty(index - 1)
-                ? cumulativeSums[index - 1]
-                : Infinity;
-    return grandTotal - curSum === previousSum;
-  });
-  return foundIndex > -1 ? foundIndex - 1 : -1;
+
+let arrayTraversal = (arr, callBack) => {
+  arr.forEach(callBack);
 }
 
-/* it('works?', function() {
- *   let arr = [-1, 3, -4, 5, 1, -6, 2, 1];
- *   solution(arr).should.equal(1);
- * });
- * it('what if it is empty?', function() {
- *   let arr = [];
- *   solution(arr).should.equal(-1);
- * });
- * it('what if it is an array of 1', function() {
- *   let arr = [1];
- *   solution(arr).should.equal(0);
- * });
- * it('2 elements', function() {
- *   let arr2 = [1, 0];
- *   solution(arr2).should.equal(0);
- * });*/
+describe('this is very important', function() {
+  it('should go through', function() {
+    let arr = [1,2,3,4,5];
+    let amount = 0;
+    arrayTraversal(arr, () => amount++);
+    amount.should.equal(arr.length);
+  });
+  it('hey ther', function() {
+    let arr = [];
+    let amount = 0;
+    arrayTraversal(arr, () => amount++)
+    amount.should.equal(0);
+  });
+});
