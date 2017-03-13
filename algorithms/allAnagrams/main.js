2eb7e1e8e179c761c78392f96fbdc2d433e7d9ef
way0750 <bruce0750@gmail.com>
2017-03-13 12:25:16 -0700

    got the time and space complexity for the allAnagrams

--- algorithms/allAnagrams/main.js
+++ algorithms/allAnagrams/main.js
@@ -26,4 +26,64 @@
    to its every position
    for example: if so we have 'a' as sub permutation, we will enter b to each
    position: ba then ab
+
+   pseudo code:
+   if using recursion:
+   base case: input is empty string: return  ['']; symbolize as have one and only
+     permutation
+   how to make problem smaller: slice string smaller and smaller, eventually it
+     will become an empty string
+   what to return always: an array, which is an array of strings aka permutations
+   what to do with those returns: for each string in the returned array, add
+     current char to each positon in the string, which yields a new permutation
+
+   time and space complexity
+     time:
+     ''
+      'a' 1
+       'ab' 'ba' 2
+       'cab' ' acb' ' abc' 'cba' 'bca' 'bac' 6
+        '4 new permutations for each'
+   (last length + 1) * last amount
+   so 6 * (4) = 24?
+   last amount * (depth) === amount at depth
+
+   1 * (1 +0 ) = 1
+   1 * ( 2 ) = 2
+   2 * (3) = 6
+   6 * (4) = 24
+   total = 1 + 2 + 6 + 24 = 33
+   (((1 * (1)) * ( 2 )) * (3)) * (4)
+   so basically !depth? to get the last depth that is
+   1 * 1 * 2 * 3 * 4
+   !1 + !2 + !3 + !4 + !5 + !n
+
+   depth = n
+   -------
+   <        !depth
+   ------
+   depth = 1
+
+   but you have to go through all the return permutations to create new ones
+
+   depth = n - 1
+   -------
+   <        !depth
+   ------
+   depth = 1
+
+   put the together, drop the constant, you end up with:
+
+   depth = n
+   -------
+   <        !depth
+   ------
+   depth = 1
+
+   space complexity
+   will be !depth amount of permuatation eventually, so !depth
+   + the stack size which is depth, but it is non dominent, so drop it
+   you end up with !depth
+
+   if doing this in an iteractive way, then the time and space will be the same
 */
