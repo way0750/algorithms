79b9eb9beeeedcc9a040b8f221d7e1ff536f9e14
way0750 <bruce0750@gmail.com>
2017-03-13 11:48:33 -0700

    got basic idea for allAnagram

--- algorithms/allAnagrams/main.js
+++ algorithms/allAnagrams/main.js
@@ -11,4 +11,19 @@
  * example usage:
  * var anagrams = allAnagrams('abc');
  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+   so basically permutations on input
  */
+
+
+/*
+   given a strin as input, then produce all permutations and output in array
+   abc
+   a: a
+   b: ba, ab // place b in all possibel position, front mid and end
+   c: cba, bca, bac, cab, acb, abc // place c in all possible possible position
+     in ba, then in ab
+   so the basic idea is for each and every sub permutation, insert a new letter
+   to its every position
+   for example: if so we have 'a' as sub permutation, we will enter b to each
+   position: ba then ab
+*/
