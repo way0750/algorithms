f89d6378c68fdacb8d29da47c391f178a43d7096
way0750 <bruce0750@gmail.com>
2017-03-16 10:05:26 -0700

    longestsubstring recursion done but dont use it with long strings

--- algorithms/Longest Substring Without Repeating Characters/main.js
+++ algorithms/Longest Substring Without Repeating Characters/main.js
@@ -33,6 +33,18 @@
          unless current index is same as length
 
    return curMax;
+
+   time and space:
+   looping through the string: n
+   then each time you found a repeated char, you need to clear and recreate the
+     record: at worst, you will need to recreate k times for k repeatd chars,
+     and loop l iterations to recreate the record
+     so total n + k * l
+
+   for space:
+     record would take the length of the longest sub string if we only use 255 chars
+       then it would be at worst 255 chars big
+     curStart and max are constant
 */
 
 let longestUniqSubstring = function(string) {
@@ -60,3 +72,41 @@ let longestUniqSubstring = function(string) {
   return curMax;
 };
 
+/*
+   what if I have to use recursion?
+   when found repeativie chars calculate length and recursively call?
+   just pass start and end index
+   when repeat found or at end, calculat length, clear record, recursively call
+
+   base case: at the end of string, curIndex is same as string length
+     calculat substring length
+   how to make problem smaller: pass repeat char first occurency + 1 as start
+     pass cur index as curIndex;
+     recreate record form curStart to curIndex;
+   what to return: a number
+   what to with returns: return the max;
+*/
+
+//working but don't use it with long string due the limites on recursion amount
+let longestUniqSubstringRecursion = function(string, curStart = 0, curIndex = 0) {
+  if (curStart >= string.length) return 0;
+
+  let record = {};
+  for(let i = curStart; i < curIndex; i++) {
+    let char = string[i];
+    record[char] = i;
+  }
+
+  let char = string[curIndex];
+  while (!record.hasOwnProperty(char) && curIndex < string.length) {
+    record[char] = curIndex;
+    curIndex++;
+    char = string[curIndex];
+  }
+  let curMax = curIndex - curStart;
+  // get the next start index, but if at the end of stirng already
+  // then char would be undefined and + 1 would yeild NaN, then assgin
+  // curIndex instead
+  curStart = record[char] + 1 || curIndex;
+  return Math.max(curMax, longestUniqSubstring(string, curStart, curIndex));
+};
--- algorithms/Longest Substring Without Repeating Characters/tests.js
+++ algorithms/Longest Substring Without Repeating Characters/tests.js
@@ -5,4 +5,8 @@ describe('', function() {
     longestUniqSubstring('bbbbb').should.equal(1)
     longestUniqSubstring('pwwkew').should.equal(3);
   });
+  it('this might take very long:', function() {
+    let str = 'abcdefghijklmnopqrstuvwxyz'.repeat(2600);
+    longestUniqSubstring(str).should.equal(26);
+  });
 });
