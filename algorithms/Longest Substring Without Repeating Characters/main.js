7cdec27ff49d610237309f7597c13f43897a614d
way0750 <bruce0750@gmail.com>
2017-03-16 09:10:34 -0700

    psudo code done. gonna solve it in a iteractive way

--- algorithms/Longest Substring Without Repeating Characters/main.js
+++ algorithms/Longest Substring Without Repeating Characters/main.js
@@ -14,4 +14,27 @@
 
    Given "pwwkew", the answer is "wke", with the length of 3. Note that the
      answer must be a substring, "pwke" is a subsequence and not a substring.
+
+   abcdacdceaec
+   set record to {}
+   set curStart to 0
+   set curMax to 0
+   loop from 1 to <= string length
+     set curChar to current character
+     if curChar is not found in record
+       add curChar as key, current index as value
+     if curChar is found in record or current index is string length
+       time to calculate the length of substring by current index - curStart
+       assign the result to curMax if larger
+       assign the found repeat char's first occurent index from record
+       to curStart
+       assign {} to record
+       loop from curStart to current index and ass each char and their index;
+         unless current index is same as length
+
+   return curMax;
 */
+
+let longestUniqSubstring = function(string) {
+  
+}
