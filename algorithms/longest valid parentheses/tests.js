32d80f3f7fa94bba49edf9100a06017ef3443b85
way0750 <bruce0750@gmail.com>
2017-03-19 19:14:14 -0700

    binary tree iterator

--- algorithms/longest valid parentheses/tests.js
+++ algorithms/longest valid parentheses/tests.js
@@ -1,12 +1,12 @@
 chai.should();
 describe('', function() {
-  it('longestValidParent 1', function() {
+  qt(function() {
     longestValidParent('()').should.equal(2);
   });
-  it('longestValidParent 2', function() {
+  qt(function() {
     longestValidParent(')()()()').should.equal(6);
   });
-  it('longestValidParent 3', function() {
+  qt(function() {
     longestValidParent(')(()()())').should.equal(8);
   })
 });
