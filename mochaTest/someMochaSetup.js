320ced5750cdf7dd3dc3309d7499c0b05f8f95c4
way0750 <bruce0750@gmail.com>
2017-03-19 17:45:50 -0700

    add longest valid parentheses

--- mochaTest/someMochaSetup.js
+++ mochaTest/someMochaSetup.js
@@ -2,3 +2,30 @@
    most if not all the set up things go here
 */
 chai.should();
+
+let QuickTest = () => {
+  let testID = 1;
+
+  this.qt = function(arg1, arg2) {
+    let testName = `Test Number: ${testID++}`;
+    let callBack;
+    if (typeof arg1 === 'string') {
+      testName = arg1;
+      callBack = arg2
+    } else if (typeof arg1 === 'function') {
+      callBack = arg1;
+    } else {
+      callBack = () => {
+        ('Did you provide a callBack function?').should.to.be.true
+      }
+    }
+    it(testName, callBack);
+  }
+
+  this.xqt = function(arg1) {
+    let testName = typeof arg1 === 'string' ? arg1 : `Test Number: ${testID++}`;
+    xit(testName, () => {});
+  }
+}
+
+QuickTest();
