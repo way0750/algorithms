3c18b3f2255d8b4fc3cd5c42322c8b984906f22b
way0750 <bruce0750@gmail.com>
2017-03-20 14:05:07 -0700

    added with smallest amount of coins

---
+++ algorithms/change with smallest amount of coins/main.js
@@ -0,0 +1,8 @@
+/*
+   given an array of coins (in numbers) sorted in acending order
+   given a number, value
+   find a smallest amount of coins to make up to that value
+     return a number basically
+   ex: coins [20, 15, 10, 5, 4, 1]; value: 23
+   return 3 because one way is: 15 4 4
+*/
---
+++ algorithms/change with smallest amount of coins/someSetup.js
@@ -0,0 +1,26 @@
+let QuickTest = () => {
+  let testID = 1;
+
+  this.qt = function(arg1, arg2) {
+    let testName = `Test Number: ${testID++}`;
+    let callBack;
+    if (typeof arg1 === 'string') {
+      testName = arg1;
+      callBack = arg2
+    } else if (typeof arg1 === 'function') {
+      callBack = arg1;
+    } else {
+      callBack = () => {
+        ('Did you provide a callBack function?').should.to.be.true
+      }
+    }
+    it(testName, callBack);
+  }
+
+  this.xqt = function(arg1) {
+    let testName = typeof arg1 === 'string' ? arg1 : `Test Number: ${testID++}`;
+    xit(testName, () => {});
+  }
+}
+
+QuickTest();
