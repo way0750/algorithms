2d49ec038cb20e1242cb199c91f8ba0ba98d7703
way0750 <bruce0750@gmail.com>
2017-03-24 15:32:59 -0700

    minPath done

--- algorithms/Triangle/main.js
+++ algorithms/Triangle/main.js
@@ -23,3 +23,20 @@ The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
    eventually you will end up with an array of 1 element or none
    if 1 element, return it, if nothing then return Infinity
 */
+
+// soooooo this is dynamic programming?
+let minPath = function(triangle) {
+  let min = triangle.reduceRight((subMinArr, curArr) => {
+    let addedArr = curArr.map((_, i) => curArr[i] + (subMinArr[i] || 0));
+
+    if (addedArr.length === 1) return addedArr;
+
+    return Array(addedArr.length - 1)
+      .fill(0)
+      .map((_, i) => Math.min(addedArr[i], addedArr[i+1]));
+  }, []);
+
+  return min.length ? min[0] : Infinity;
+};
+
+//
--- algorithms/Triangle/tests.js
+++ algorithms/Triangle/tests.js
@@ -1,6 +1,21 @@
 chai.should();
 describe('', function() {
-  it('', function() {
-    // hey().should.be.a('string');
+  it('minPath', function() {
+    let tri = [
+      [2],
+      [3,4],
+      [6,5,7],
+      [4,1,8,3]
+    ];
+    minPath(tri).should.equal(11);
+  });
+  it('minPath', function() {
+    let tri = [
+      [2],
+      [0,4],
+      [6,5,7],
+      [4,1,8,3]
+    ];
+    minPath(tri).should.equal(8);
   });
 });
