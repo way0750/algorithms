5b466b8ba95a319d955f66bf3b50e3076a02dcd8
way0750 <bruce0750@gmail.com>
2017-03-24 17:00:18 -0700

    unique paths ii done

--- algorithms/Unique Paths ii/main.js
+++ algorithms/Unique Paths ii/main.js
@@ -1,16 +1,42 @@
 /*
-Now consider if some obstacles are added to the grids. How many unique paths
-   would there be?
 
-An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+  Now consider if some obstacles are added to the grids. How many unique paths
+    would there be?
 
-For example,
-There is one obstacle in the middle of a 3x3 grid as illustrated below.
+  An obstacle and empty space is marked as 1 and 0 respectively in the grid.
 
-[
-  [0,0,0],
-  [0,1,0],
-  [0,0,0]
-]
-The total number of unique paths is 2.
+  For example,
+  There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+  [
+    [0,0,0],
+    [0,1,0],
+    [0,0,0]
+  ]
+  The total number of unique paths is 2.
+
+  do the same this time but without inverting the grid
+  and set the table to default value { xlegnth: 1 } for the ending cell
+   for each cell check and see if there is a mark there already
+   if yes then value is 0;
+   if no then calculate value by adding right + bottom and if nothing found use0
+
+   at the end return value at index 00 or simply 0
 */
+
+let uniquePathsII = function(grid){
+  let xLength = (grid[0] || []).length;
+  let yLength = (grid || []).length;
+  let table = grid.reduceRight((record, row, y) => {
+    for (let x = row.length - 1; x > -1; x--) {
+      let key = `${x}:${y}`;
+      let rightVal = record[`${x+1}:${y}`] || 0;
+      let bottomVal = record[`${x}:${y+1}`] || 0;
+      let value = row[x] === 'x' ? 0 : rightVal + bottomVal;
+      record[key] = value;
+    }
+    return record;
+  }, { [`${xLength}:${yLength-1}`]: 1 });
+
+  return table['0:0'] || 0;
+};
--- algorithms/Unique Paths ii/tests.js
+++ algorithms/Unique Paths ii/tests.js
@@ -1,6 +1,43 @@
 chai.should();
 describe('', function() {
-  it('', function() {
-    // hey().should.be.a('string');
+  it('unique paths ii', function() {
+    let grid = [
+      [0,0,0],
+      [0,'x',0],
+      [0,0,0],
+    ];
+    uniquePathsII(grid).should.equal(2)
+  });
+  it('empty grid should return 0', function() {
+    uniquePathsII([]).should.equal(0);
+  });
+  it('a big one', function() {
+    let grid = [
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+    ];
+    uniquePathsII(grid).should.equal(75582);
+  });
+  it('another one', function() {
+    let x = 'x';
+    let grid = [
+      [0,0,0,0,0,0,0,0,0,0,0,x],
+      [0,0,0,0,0,0,x,0,0,0,0,0],
+      [0,0,0,0,x,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,x,0,0,0],
+      [0,0,0,0,0,0,0,0,0,x,0,x],
+      [0,0,0,0,x,0,x,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0,0,0],
+    ]
+    uniquePathsII(grid).should.equal(14889);
   });
 });
