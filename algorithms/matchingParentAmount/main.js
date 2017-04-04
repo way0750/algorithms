98615b68aa7296657dd739d520da5264c973a4e1
way0750 <bruce0750@gmail.com>
2017-04-03 19:11:58 -0700

    pseudo code basically done

--- algorithms/matchingParentAmount/main.js
+++ algorithms/matchingParentAmount/main.js
@@ -10,4 +10,19 @@
      you will end up with these two sub strings:
    (((( has 4 (
    ()()(()) has 4 )
+
+
+   solution:
+   set lastIndexOfMatchingAmount to -1;
+   set a while loop with condition that left pointer is smaller than right one
+     for each loop
+       keep two pointers: one points at a ( from the left, one points at a )
+       in the loop move each point in their own while until finding the next
+         respective ( or  )
+         if found set lastIndexOfMatchingAmount to left index;
+
+   return lastIndexOfMatchingAmount > -1 ? lastIndexOfMatchingAmount + 1 : -1;
 */
+
+
+
