b735b20b835ff351b0e81c282261cb6157bcd413
way0750 <bruce0750@gmail.com>
2017-04-07 16:41:15 -0700

    findMissingRanges done

--- algorithms/missing ranges/main.js
+++ algorithms/missing ranges/main.js
@@ -4,3 +4,18 @@
    For example, given [0, 1, 3, 50, 75],
    return [“2”, “4->49”, “51->74”, “76->99”]
 */
+
+
+let findMissingRanges = function(nums) {
+  if (!nums.length) return ['0->99'];
+
+  return nums.reduce((arr, curNum, index) => {
+    let nextNum = nums.hasOwnProperty(index + 1) ? nums[index+1] : 100;
+    if (nextNum - curNum === 2) {
+      arr.push(`${curNum+1}`);
+    } else if (nextNum - curNum > 1) {
+      arr.push(`${curNum + 1}->${nextNum-1}`);
+    }
+    return arr;
+  }, []);
+}
--- algorithms/missing ranges/tests.js
+++ algorithms/missing ranges/tests.js
@@ -1,6 +1,11 @@
 chai.should();
 describe('', function() {
-  it('', function() {
-    // hey().should.be.a('string');
+  it('empty array? should return ["0->99"]', function() {
+    findMissingRanges([]).should.deep.equal(['0->99']);
+  });
+  it('works if not empty', function() {
+    let arr = [0, 1, 3, 50, 75];
+    let expected = ['2', '4->49', '51->74', '76->99'];
+    findMissingRanges(arr).should.deep.equal(expected);
   });
 });
