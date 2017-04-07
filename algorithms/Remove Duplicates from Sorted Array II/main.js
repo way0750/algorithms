f6d65e11544178fd23b7e4adcd0cc13de20b1d96
way0750 <bruce0750@gmail.com>
2017-04-07 13:54:37 -0700

    remove duplicates done

--- algorithms/Remove Duplicates from Sorted Array II/main.js
+++ algorithms/Remove Duplicates from Sorted Array II/main.js
@@ -8,4 +8,30 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums
    being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the
    new length.
+
+   solution:
+   set foundDupForCurNum to false;
+   set length to 0;
+   loop thorugh the array and compare current number to last number
+   if the same and foundDupForCurNum is false then length++;
+   if not the same then length++ and set foundDupForCurNum to false;
+
+   return length;
 */
+
+let removeDupplicates = function(arr) {
+  let record = arr.reduce((record, curNum, index) => {
+    let preNum = arr.hasOwnProperty(index - 1) ? arr[index-1] : -Infinity;
+    if (curNum === preNum && record.firstDup) {
+      record.length++;
+      record.firstDup = false;
+    } else if (curNum !== preNum) {
+      record.length++;
+      record.firstDup = true;
+    }
+
+    return record;
+  }, { length: 0, firstDup: true });
+
+  return record.length;
+};
--- algorithms/Remove Duplicates from Sorted Array II/tests.js
+++ algorithms/Remove Duplicates from Sorted Array II/tests.js
@@ -1,6 +1,12 @@
 chai.should();
 describe('', function() {
-  it('', function() {
-    // hey().should.be.a('string');
+  it('should return 6', function() {
+    let arr = [1,1,1,1,1,1,1,2,2,3,3];
+    removeDupplicates(arr).should.equal(6);
   });
+
+  it('return 0 for empty array', function() {
+    let arr = [];
+    removeDupplicates(arr).should.equal(0);
+  })
 });
